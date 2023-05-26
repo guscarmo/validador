9b523287abd6114299367c73009694de7973d176
guscarmo <gu.carmo2@gmail.com>
2023-05-26 01:22:20 -0300

    add funções

--- main.py
+++ main.py
@@ -9,134 +9,144 @@ import os
 import json
 import subprocess
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--user-data-dir=C:/Users/Gustavo/AppData/Local/Google/Chrome/User Data")
-chrome_options.add_argument("--profile-directory=Default")
 
-driver = webdriver.Chrome('C:/Users/Gustavo/Desktop/automate/chromedriver.exe', options=chrome_options)
-
-
-env_files = ["a.env", "b.env"]
-
-for env_file in env_files:
-
-    env = {}
-
+def load_environment_variables(env_file):
+    #env = {}
     load_dotenv(env_file, override=True)
-
-    env.update(os.environ)
-
-
-    pd.set_option('display.max_columns', None)  # Exibir todas as colunas
-    pd.set_option('display.expand_frame_repr', False)  # Não
+    env = dict(os.environ)
+    return env
 
 
-
-    load_dotenv(env_file)
-    publisher = os.getenv('publisher')
-    linkSheet = os.getenv('linkSheet')
-    lastRowCell = os.getenv('lastRowCell')
-    rowDays = os.getenv('rowDays')
-    Metric1 = os.getenv('Metric1')
-    Metric2 = os.getenv('Metric2')
-    Metric3 = os.getenv('Metric3')
-
-    #Arquivo .env
-    print(f"Arquivo .env: {env_file}")
-
-    #Forma o link da celula que contem o valor da ultima linha preenchida
-    rowDays = int(rowDays)
-    linkLastRow = f'{linkSheet}&range={lastRowCell}'
-
-    #Acessa o link
-    driver.get(linkLastRow)
+def access_last_row_link(driver, link_last_row):
+    driver.get(link_last_row)
     sleep(2)
-
-    #Copia para area de tranferencia e forma o link das ultimas 7 linhas
     a = ActionChains(driver)
     a.key_down(Keys.CONTROL).send_keys('C').key_up(Keys.CONTROL).perform()
-    numberLastRow = pyperclip.paste()
-    numberFirstRowIndex = int(numberLastRow) - rowDays
-    linkInterval = f'{linkSheet}&range=A{numberFirstRowIndex}:D{numberLastRow}'
 
-    #Acessa o intervalo das ultimas 7 linhas e copia
-    driver.get(linkInterval)
+
+def access_interval_link(driver, link_interval):
+    driver.get(link_interval)
+    a = ActionChains(driver)
     a.key_down(Keys.CONTROL).send_keys('C').key_up(Keys.CONTROL).perform()
 
-    #Inicia o df através da area de tranferencia
-    df_sheet = pd.read_clipboard()
 
-    #Define o nome das colunas
-    column_names = ['DATE', Metric1, Metric2, Metric3]
-    df_sheet.columns = column_names
+def get_dataframe_from_clipboard():
+    df = pd.read_clipboard()
+    return df
 
-    #Formata a data
-    df_sheet['DATE'] = pd.to_datetime(df_sheet['DATE'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%Y-%m-%d')
+def format_dataframe(df, metric_columns):
+    df.columns = ['DATE'] + metric_columns
+    df['DATE'] = pd.to_datetime(df['DATE'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%Y-%m-%d')
+    df = df.replace(',', '.', regex=True)
+    for column in metric_columns:
+        df[column] = df[column].astype(float)
+    return df
 
-    #Substitui e formata valores
-    df_sheet = df_sheet.replace(',', '.', regex=True)
-
-    df_sheet[Metric1] = df_sheet[Metric1].astype(float)
-    df_sheet[Metric2] = df_sheet[Metric2].astype(float)
-    df_sheet[Metric3] = df_sheet[Metric3].astype(float)
-
-
-    #Inicia o segundo df que vai ser comparado
-    df_bq = pd.read_excel('bq.xlsx', sheet_name='Planilha1', header=1, engine='openpyxl')
-    column_names = ['DATE', Metric1, Metric2, Metric3]
-    df_bq.columns = column_names
-
-    df_bq['DATE'] = pd.to_datetime(df_bq['DATE'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%Y-%m-%d')
-
-    df_bq = df_bq.replace(',', '.', regex=True)
-
-    df_bq[Metric1] = df_bq[Metric1].astype(float)
-    df_bq[Metric2] = df_bq[Metric2].astype(float)
-    df_bq[Metric3] = df_bq[Metric3].astype(float)
-
-    #Mescla os 2 df
+def merge_dataframes(df_sheet, df_bq, metric_columns):
     df_merge = pd.merge(df_sheet, df_bq, on='DATE', suffixes=('_sheet', '_bq'))
-
-    #Calcula a diferença entre os dois df
-    def difference(row):
-        diffs = []
-        for column in [Metric1, Metric2, Metric3]:
-            col1 = f'{column}_sheet'
-            col2 = f'{column}_bq'
-            try:
-                diff_pct = ((row[col2] - row[col1]) / row[col1]) * 100
-                if abs(diff_pct) > 5:
-                    diffs.append({
-                        'DATE': row['DATE'],
-                        'Dimensão': column,
-                        'Diferença (%)': diff_pct
-                    })
-            except KeyError:
-                continue
-        return diffs
-
-    resulted = df_merge.apply(difference, axis=1)
-
-    df_resulted = pd.DataFrame([item for sublist in resulted for item in sublist])
-    df_resulted.name = f'Diferenças para o veículo {publisher}'
-
-    dates = df_resulted['DATE'].unique()
-    dates_line = ' '.join(dates)
+    return df_merge
 
 
-    print(f"{df_resulted.name}\n\
-    {df_resulted}\n\
-    Datas distintas: {dates_line}")
+def calculate_differences(row, metric_columns):
+    diffs = []
+    for column in metric_columns:
+        col1 = f'{column}_sheet'
+        col2 = f'{column}_bq'
+        try:
+            diff_pct = ((row[col2] - row[col1]) / row[col1]) * 100
+            if abs(diff_pct) > 5:
+                diffs.append({
+                    'DATE': row['DATE'],
+                    'Dimensão': column,
+                    'Diferença (%)': diff_pct
+                })
+        except KeyError:
+            continue
+    return diffs
 
-    df_resulted_dict = df_resulted.to_dict(orient='records')
 
-    output_data = {
-        'name': df_resulted.name,
-        'result': df_resulted_dict,
-        'dates': dates_line
-    }
+def generate_output_data(df_resulted, publisher):
+    if df_resulted.empty:
+        output_data = {
+            'name': f'{publisher} - Ok',
+            'result': [],
+            'dates': ''
+        }
+    else:
+        dates = df_resulted['DATE'].unique()
+        dates_line = ' '.join(dates)
+        output_data = {
+            'name': f'Diferenças para o veículo {publisher}',
+            'result': df_resulted.to_dict(orient='records'),
+            'dates': dates_line
+        }
+    return output_data
+#
+# print(f"{df_resulted.name}\n\
+# {df_resulted}\n\
+# Datas distintas: {dates_line}")
 
+#df_resulted_dict = df_resulted.to_dict(orient='records')
+
+def save_output_data(output_data, output_file):
     with open('resultados.json', 'w') as json_file:
         json.dump(output_data, json_file, indent=4)
 
-    subprocess.run(['python', 'botDiscord.py'])
+
+def main():
+    env_files = ["a.env", "b.env"]
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--user-data-dir=C:/Users/Gustavo/AppData/Local/Google/Chrome/User Data")
+    chrome_options.add_argument("--profile-directory=Default")
+
+    for env_file in env_files:
+        env = load_environment_variables(env_file)
+
+        pd.set_option('display.max_columns', None)  # Exibir todas as colunas
+        pd.set_option('display.expand_frame_repr', False)  # Não
+
+        publisher = env.get('PUBLISHER')
+        linkSheet = env.get('LINKSHEET')
+        lastRowCell = env.get('LASTROWCELL')
+        rowDays = int(env.get('ROWDAYS'))
+        metric_columns = [env.get('METRIC1'), env.get('METRIC2'), env.get('METRIC3')]
+
+
+        driver = webdriver.Chrome('C:/Users/Gustavo/Desktop/automate/chromedriver.exe', options=chrome_options)
+
+        linkLastRow = f'{linkSheet}&range={lastRowCell}'
+        access_last_row_link(driver, linkLastRow)
+
+        numberLastRow = pyperclip.paste()
+
+        numberFirstRowIndex = int(numberLastRow) - rowDays
+        linkInterval = f'{linkSheet}&range=A{numberFirstRowIndex}:D{numberLastRow}'
+        access_interval_link(driver, linkInterval)
+
+        df_sheet = get_dataframe_from_clipboard()
+        df_sheet = format_dataframe(df_sheet, metric_columns)
+
+
+        df_bq = pd.read_excel('bq.xlsx', sheet_name='Planilha1', header=1, engine='openpyxl')
+        df_bq = format_dataframe(df_bq, metric_columns)
+
+
+        df_merge = merge_dataframes(df_sheet, df_bq, metric_columns)
+        print(df_merge)
+
+        resulted = df_merge.apply(calculate_differences, axis=1, args=(metric_columns,))
+        df_resulted = pd.DataFrame([item for sublist in resulted for item in sublist])
+
+
+        output_data = generate_output_data(df_resulted, publisher)
+        save_output_data(output_data, 'resultados.json')
+
+        subprocess.run(['python', 'botDiscord.py'])
+
+        driver.quit()
+
+if __name__ == "__main__":
+    main()
+
+
